717a3b7dbb9e53c6a491b42aaa0d59e259aff465
Messai <messai@github.com>
2023-10-05 15:04:01 +0300

    2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines function floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Computes the floor of a float
+    Returns:
+        The float value of the input
+    """
+    return int(n)

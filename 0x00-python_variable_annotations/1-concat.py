b887a4130b55947aff8c0a59de9e7aeabf10ef5d
Messai <messai@github.com>
2023-10-05 14:58:53 +0300

    1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Defines function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings together
+    Returns:
+         The concatenated string
+    """
+    return str1 + str2

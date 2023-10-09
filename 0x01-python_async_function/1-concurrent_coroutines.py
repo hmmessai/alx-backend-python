ecbc68822f6eb28371640b5ea4b42a673fc67f38
Messai <messai@github.com>
2023-10-09 21:17:36 +0300

    another 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
 Defines the asyncronous funtion wait_n
 """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Creates a list of wait_random process return
     with ascending order

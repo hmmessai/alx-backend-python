cd44effdaa4dae5c0abf5c93f9aa49a7ef1b51d5
Messai <messai@github.com>
2023-10-09 15:54:52 +0300

    1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Defines an asynchrounous coroutine wait_random
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    waits for a random delay between 0 and max_delay
+    Returns: the time it waited.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay

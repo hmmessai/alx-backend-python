3a14747b0a264fb609090e2adfb62a3aaa50ced9
Messai <messai@github.com>
2023-10-10 15:01:27 +0300

    1

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[int, None, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Yields 10 random number between 0 and 10
     with 1 second wait

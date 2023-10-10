644f50314945beb88307d507799e2280c1111ae1
Messai <messai@github.com>
2023-10-10 17:12:22 +0300

    2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """
 Define an async_comprehension function
 """
-import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """
     Collect 10 random numbers using async_generator
     and return the 10 random numbers
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
 """
+Define measure_runtime coroutine
+"""
+import asyncio
+from time import perf_counter
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Executes async_comprehension four times
+    in parallel and returns the runtime
+    """
+    start = perf_counter()
+    task = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*task)
+    runtime = perf_counter() - start
+
+    return runtime

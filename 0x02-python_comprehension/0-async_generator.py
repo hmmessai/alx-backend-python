b1519c78a47f20af78cc8bf1c0714b23690f7270
Messai <messai@github.com>
2023-10-10 14:39:57 +0300

    0

---
+++ 0x02-python_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Define async_generator funciton
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Yields 10 random number between 0 and 10
+    with 1 second wait
+    """
+    for i in range(10):
+        yield random.uniform(0, 10)
